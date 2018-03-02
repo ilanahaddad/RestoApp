e0d1fa39826202ebdb3cbe44fcb8da6e50a6f811
camilogarcialarotta <camilo.garcia.larotta@gmail.com>
2018-03-02 11:22:09 -0500

    Panel updates with every table added

--- RestoApp/src/ca/mcgill/ecse223/resto/view/RestoAppPage.java
+++ RestoApp/src/ca/mcgill/ecse223/resto/view/RestoAppPage.java
@@ -1,11 +1,17 @@
 package ca.mcgill.ecse223.resto.view;
 
+import ca.mcgill.ecse223.resto.controller.InvalidInputException;
+import ca.mcgill.ecse223.resto.controller.RestoController;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.*;
+
+import static java.lang.Integer.parseInt;
 
 
 public class RestoAppPage extends JFrame
@@ -14,6 +20,8 @@ public class RestoAppPage extends JFrame
     private final String GUI_TITLE = "RestoApp";
     private final int GUI_WIDTH = 500;
     private final int GUI_HEIGHT = 450;
+
+    private JPanel tablePanel;
 
     public RestoAppPage() { initUI(); }
 
@@ -31,7 +39,7 @@ public class RestoAppPage extends JFrame
 
     private void createTablePanel()
     {
-        TablePanel tablePanel = new TablePanel();
+        tablePanel = new TablePanel();
         add(tablePanel);
     }
 
@@ -40,9 +48,9 @@ public class RestoAppPage extends JFrame
         JMenuBar menubar = new JMenuBar();
         JMenu file = new JMenu("Actions");
         JMenuItem addTableMenuItem = createMenuItem(
-                "Add Table", KeyEvent.VK_A, RestoAppForms.ADD_TABLE_ACTION);
+                "Add Table", KeyEvent.VK_A, this::addTableAction);
         JMenuItem exitMenuItem = createMenuItem(
-                "Exit", KeyEvent.VK_E, RestoAppForms.EXIT_ACTION);
+                "Exit", KeyEvent.VK_E, RestoAppActions.EXIT_ACTION);
 
         file.setMnemonic(KeyEvent.VK_F);
         file.add(addTableMenuItem);
@@ -63,9 +71,9 @@ public class RestoAppPage extends JFrame
     {
         JToolBar toolbar = new JToolBar();
         JButton exitButton = createButton(
-                "power.png", "Exit RestoApp", RestoAppForms.EXIT_ACTION);
+                "power.png", "Exit RestoApp", RestoAppActions.EXIT_ACTION);
         JButton addTableButton = createButton(
-                "addTable.png", "Add Table", RestoAppForms.ADD_TABLE_ACTION);
+                "addTable.png", "Add Table", this::addTableAction);
 
         toolbar.add(exitButton);
         toolbar.add(addTableButton);
@@ -80,5 +88,62 @@ public class RestoAppPage extends JFrame
         button.setToolTipText(buttonTip);
         button.addActionListener(action);
         return button;
+    }
+
+    private void addTableAction(ActionEvent event)
+    {
+        JPanel panel = new JPanel(new GridLayout(6, 2, 5,5));
+
+        panel.add(new JLabel("Table Number:"));
+        JTextField tableNumField = new JTextField();
+        panel.add(tableNumField);
+
+        panel.add(new JLabel("Number of Seats:"));
+        JTextField numSeatsField = new JTextField();
+        panel.add(numSeatsField);
+
+        panel.add(new JLabel("X:"));
+        JTextField xField = new JTextField();
+        panel.add(xField);
+
+        panel.add(new JLabel("Y:"));
+        JTextField yField = new JTextField();
+        panel.add(yField);
+
+        panel.add(new JLabel("Width:"));
+        JTextField widthField = new JTextField();
+        panel.add(widthField);
+
+        panel.add(new JLabel("Length:"));
+        JTextField lengthField = new JTextField();
+        panel.add(lengthField);
+
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Add Table",
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        if (result == JOptionPane.OK_OPTION)
+        {
+            //TODO validate input
+            int numSeats = parseInt(numSeatsField.getText());
+            int tableNum = parseInt(tableNumField.getText());
+            int x = parseInt(xField.getText());
+            int y = parseInt(yField.getText());
+            int width = parseInt(widthField.getText());
+            int length = parseInt(lengthField.getText());
+            try {
+                RestoController.createTableAndSeats(numSeats, tableNum, x, y, width, length);
+            } catch (InvalidInputException e) {
+                e.printStackTrace();
+            }
+
+            tablePanel.revalidate();
+            tablePanel.repaint();
+
+            String successMessage = "Added table #" + tableNum + " with " + numSeats + " seats.";
+            JOptionPane.showMessageDialog(null, successMessage);
+
+        } else {
+            JOptionPane.showMessageDialog(null, "No Table Added.");
+        }
     }
 }
