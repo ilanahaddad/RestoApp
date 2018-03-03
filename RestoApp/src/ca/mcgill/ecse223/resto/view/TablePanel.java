4f6c9bdf4d3ccee02dbe86f48b1a97ada2ffa920
camilogarcialarotta <camilo.garcia.larotta@gmail.com>
2018-03-02 22:39:22 -0500

    Seats now added on the sides too

--- RestoApp/src/ca/mcgill/ecse223/resto/view/TablePanel.java
+++ RestoApp/src/ca/mcgill/ecse223/resto/view/TablePanel.java
@@ -79,28 +79,99 @@ public class TablePanel extends JPanel
 
     private void drawSeats(Table table, Graphics2D g2d)
     {
-        int x = table.getX();
-        int y = table.getY();
-        int width = table.getWidth();
-        int maxSeatsPerLine = (width*UNIT_LENGTH - 15)/ (seatDiameter);
+        int totalNumSeats = table.getSeats().size();
 
-        int seatYOffset = 0;
-        int seatXOffset = 0;
-        for (int i=0;i<table.getSeats().size(); i++)
+        if (table.getWidth() >= table.getLength())
         {
-            if (i==maxSeatsPerLine)
+            int tablesLeftToDraw = fillTopBottom(table, totalNumSeats,g2d);
+            fillLeftRight(table, totalNumSeats-tablesLeftToDraw, g2d);
+            //TODO what if num chairs larger than what can be fit
+        }
+        else
+        {
+            int tablesLeftToDraw = fillLeftRight(table, totalNumSeats,g2d);
+            fillTopBottom(table, (totalNumSeats-tablesLeftToDraw), g2d);
+        }
+    }
+
+    // return number of seats drawn
+    private int fillTopBottom(Table table, int seatsToDraw, Graphics2D g2d)
+    {
+        int maxSeatsPerWidth = (table.getWidth()*UNIT_LENGTH - 15)/ (seatDiameter);
+
+        int drawnSeats = 0;
+        int seatYOffset = 0;
+        int seatXOffset = 3;
+        for (int i=0;i<seatsToDraw; i++)
+        {
+            if (i>0 && i%maxSeatsPerWidth == 0)
             {
+                if (seatYOffset == table.getLength()*UNIT_LENGTH-seatDiameter)
+                {
+                    // already filled both top and bottom
+                    return drawnSeats;
+                }
                 seatYOffset=table.getLength()*UNIT_LENGTH-seatDiameter;
-                seatXOffset=0;
+                seatXOffset=3;
             }
 
             g2d.setColor(Color.BLACK);
-            g2d.drawOval(x*UNIT_LENGTH+seatXOffset, y*UNIT_LENGTH+seatYOffset, seatDiameter, seatDiameter);
+            g2d.drawOval(
+                    table.getX()*UNIT_LENGTH+seatXOffset,
+                    table.getY()*UNIT_LENGTH+seatYOffset,
+                    seatDiameter, seatDiameter);
             g2d.setColor(Color.WHITE);
-            g2d.fillOval(x*UNIT_LENGTH+seatXOffset, y*UNIT_LENGTH+seatYOffset, seatDiameter, seatDiameter);
+            g2d.fillOval(
+                    table.getX()*UNIT_LENGTH+seatXOffset,
+                    table.getY()*UNIT_LENGTH+seatYOffset,
+                    seatDiameter, seatDiameter);
+
+            drawnSeats++;
 
             seatXOffset+=seatDiameter + seatPadding;
         }
+
+        return drawnSeats;
+    }
+
+    // return number of seats drawn
+    private int fillLeftRight(Table table, int seatsToDraw, Graphics2D g2d)
+    {
+        int maxSeatsPerLength = (table.getLength()*UNIT_LENGTH - 15)/ (seatDiameter) -2;
+
+        int drawnSeats = 0;
+        int seatYOffset = seatDiameter+seatPadding;
+        int seatXOffset = 3;
+        for (int i=0;i<seatsToDraw; i++)
+        {
+            if (i>0 && i%maxSeatsPerLength == 0)
+            {
+                if (seatXOffset == table.getWidth()*UNIT_LENGTH-seatDiameter-3)
+                {
+                    // already filled both top and bottom
+                    return drawnSeats;
+                }
+                seatXOffset=table.getWidth()*UNIT_LENGTH-seatDiameter-3;
+                seatYOffset=seatDiameter+seatPadding;
+            }
+
+            g2d.setColor(Color.BLACK);
+            g2d.drawOval(
+                    table.getX()*UNIT_LENGTH+seatXOffset,
+                    table.getY()*UNIT_LENGTH+seatYOffset,
+                    seatDiameter, seatDiameter);
+            g2d.setColor(Color.WHITE);
+            g2d.fillOval(
+                    table.getX()*UNIT_LENGTH+seatXOffset,
+                    table.getY()*UNIT_LENGTH+seatYOffset,
+                    seatDiameter, seatDiameter);
+
+            drawnSeats++;
+
+            seatYOffset+=seatDiameter + seatPadding;
+        }
+
+        return drawnSeats;
     }
 
     private Color generateRandomColor()
