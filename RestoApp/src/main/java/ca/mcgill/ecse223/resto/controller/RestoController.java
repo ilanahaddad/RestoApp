87ed5f780e5a426f5c3beeb3250c3f3d0bad0daf
camilogarcialarotta <camilo.garcia.larotta@gmail.com>
2018-03-13 09:22:12 -0400

    Implemented hasEnoughSeats for Deliverable 4

--- RestoApp/src/main/java/ca/mcgill/ecse223/resto/controller/RestoController.java
+++ RestoApp/src/main/java/ca/mcgill/ecse223/resto/controller/RestoController.java
@@ -275,6 +275,16 @@ public class RestoController
         throw new InvalidInputException("Could not retrieve table number " + tableNum);
     }
 
+    // checks if the table has enough seats to accommodate the number of clients
+    private static boolean hasEnoughSeats(int tableNum, int numSeatsRequired) 
+    {
+        Table table = null;
+        try { table = getTableByNum(tableNum); }
+        catch (InvalidInputException err) { return false; }
+        
+        return (table.getCurrentSeats().size() >= numSeatsRequired);
+    }
+
     // checks if the table with given attributes is in the set tables in the app
     private static boolean exactTableInApp(int tableNum, int numSeats, int width, int length)
     {
