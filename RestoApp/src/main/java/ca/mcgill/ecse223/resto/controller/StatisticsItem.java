52b37cc2d14a99e18f016018022ff2b4c82f6d76
PotassiumK <kikiz24301@gmail.com>
2018-04-10 22:21:50 -0400

    Add StatisticsItem helper class setters and getters

--- RestoApp/src/main/java/ca/mcgill/ecse223/resto/controller/StatisticsItem.java
+++ RestoApp/src/main/java/ca/mcgill/ecse223/resto/controller/StatisticsItem.java
@@ -1,5 +1,62 @@
 package ca.mcgill.ecse223.resto.controller;
+import ca.mcgill.ecse223.resto.model.MenuItem;
 
-public class StatisticsItem {
+public class StatisticsItem
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //StatisticsTable Attributes
+  private int numUsed;
+
+  //StatisticsTable Associations
+  private MenuItem item;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public StatisticsItem(MenuItem anItem, int aNumUsed)
+  {
+	item = anItem;
+    numUsed = aNumUsed;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setNumUsed(int aNumUsed)
+  {
+    boolean wasSet = false;
+    numUsed = aNumUsed;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public int getNumUsed()
+  {
+    return numUsed;
+  }
+
+  public boolean setItem(MenuItem anItem) {
+	  boolean wasSet = false;
+	  item = anItem;
+	  wasSet = true;
+	  return wasSet;
+  }
+  
+  public MenuItem getItem()
+  {
+    return item;
+  }
+
+  public boolean hasMenuItem()
+  {
+    boolean has = item != null;
+    return has;
+  }
 
 }
